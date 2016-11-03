0032fdf2e7013852e5a01dcdd34e993e8c4cb971
Ellie Angell and Suzi Rubino <ellie@>
2016-11-03 13:38:18 -0700

    add overall totals for both users

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // business logic
 // debugger;
 
-function Player () {
+function Player1 () {
 this.currentRoll = 0;
 this.turn= 0;
 this.gameScore= 0;
@@ -9,27 +9,58 @@ this.gameScore= 0;
 
 
 
-Player.prototype.rollDice = function() {
-
+Player1.prototype.rollDice = function() {
   var roll = Math.floor((Math.random()*6)+1);
- this.currentRoll = roll
+  this.currentRoll = roll
   if (roll > 1) {
     this.turn += roll;
   } else if (roll === 1) {
     this.turn = 0;
+    $(".playerOne").hide();
+    $(".playerTwo").show();
   }
 }
 
-Player.prototype.totalScore = function() {
-    this.gameScore = this.turn; //needs to add new turn to overall gameScore
+Player1.prototype.totalScore = function() {
+    this.gameScore = this.gameScore + this.turn;
 }
+
+
+// player 2 business logic
+
+function Player2 () {
+this.currentRoll = 0;
+this.turn= 0;
+this.gameScore= 0;
+}
+
+
+
+Player2.prototype.rollDice = function() {
+  var roll = Math.floor((Math.random()*6)+1);
+  this.currentRoll = roll
+  if (roll > 1) {
+    this.turn += roll;
+  } else if (roll === 1) {
+    this.turn = 0;
+    $(".playerTwo").hide();
+    $(".playerOne").show();
+  }
+}
+
+Player2.prototype.totalScore = function() {
+    this.gameScore = this.gameScore + this.turn;
+}
+
 
 
 // user logic
 
 $(document).ready(function() {
 
-  var newRoll = new Player();
+  var newRoll = new Player1();
+  var newRoll1 = new Player2();
+
 
   $("button#playerRoll").click(function() {
 
@@ -39,16 +70,39 @@ $(document).ready(function() {
     $("#turnTotal").text(newRoll.turn);
 
   });
-  
+
   $("button#playerHold").click(function() {
 
     newRoll.totalScore();
 
     $("#playerScore").text(newRoll.gameScore);
 
+    $(".playerOne").hide();
+    $(".playerTwo").show();
+  });
+
+  // player 2 here
+
+  $("button#playerRoll1").click(function() {
+
+    newRoll1.rollDice();
+
+    $("#rollOne1").text(newRoll1.currentRoll);
+    $("#turnTotal1").text(newRoll1.turn);
 
   });
 
-  //callback for holding
+  $("button#playerHold1").click(function() {
+
+    newRoll1.totalScore();
+
+    $("#playerTwoScore").text(newRoll1.gameScore);
+
+    $(".playerTwo").hide();
+    $(".playerOne").show();
+  });
+
+
+
 
 });
