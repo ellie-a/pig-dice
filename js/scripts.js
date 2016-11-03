fdfa32170e57cf7e476ab7c43ed51f6be0d4cec2
Ellie Angell and Suzi Rubino <suzi@>
2016-11-03 15:26:36 -0700

    added second js script to try new forming

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 // business logic
 // debugger;
 
-function Player1 () {
+function Players () {
+//this.user = playerName
+// this.playerOne = player1;
+// this.playerTwo: player2;
 this.currentRoll = 0;
 this.turn= 0;
 this.gameScore= 0;
+// this.playerTwoGameScore= 0;
 }
 
 
 
-Player1.prototype.rollDice = function() {
+Players.prototype.rollDice = function() {
 
   var roll = Math.floor((Math.random()*6)+1);
   this.currentRoll = roll
@@ -20,61 +24,39 @@ Player1.prototype.rollDice = function() {
   }
 }
 
-Player1.prototype.totalScore = function() {
+Players.prototype.totalScore = function() {
     this.gameScore = this.gameScore + this.turn;
-    if (this.gameScore >= 100){
+    // this.playerTwoGameScore = this.playerTwoGameScore + this.turn;
+    if (this.gameScore >= 100) {
       alert("Player 1 wins!");
       this.gameScore = 0;
     }
+    // else if (this.playerTwoGameScore >= 100) {
+    //   alert("Player 2 wins!");
+    //   this.playerTwoGameScore = 0;
+    //
+    // }
 }
 
-// function resetFields() {
-//
-// }
-
-// player 2 business logic
-
-function Player2 () {
-this.currentRoll = 0;
-this.turn= 0;
-this.gameScore= 0;
-}
-
-
-
-Player2.prototype.rollDice = function() {
-  var roll = Math.floor((Math.random()*6)+1);
-  this.currentRoll = roll
-  if (roll > 1) {
-    this.turn += roll;
-  } else if (roll === 1) {
-    this.turn = 0;
-  }
-}
-
-Player2.prototype.totalScore = function() {
-    this.gameScore = this.gameScore + this.turn;
-    if (this.gameScore >= 100){
-      alert("Player 2 wins!");
-      this.gameScore = 0;
-    }
-}
-
+//var playerNames = [];
 
 
 // user logic
 
 $(document).ready(function() {
 
-  var newRoll = new Player1();
-  var newRoll1 = new Player2();
+  // var playerNumber;
+  // var player = new Players (playerNumber);
+  // playerNames.push(player);
+  // var player2NewRoll = new Players (player2);
+ console.log(playerNames)
 
 
   $("button#playerRoll").click(function() {
 
-    newRoll.rollDice();
+    newroll.rollDice();
 
-    $("#rollOne").text(newRoll.currentRoll);
+    $("#player1NewRoll").text(newRoll.currentRoll);
     $("#turnTotal").text(newRoll.turn);
 
   });
@@ -86,32 +68,28 @@ $(document).ready(function() {
 
     $("#playerScore").text(newRoll.gameScore);
 
-    $(".playerOne").hide();
-    $(".playerTwo").show();
   });
 
   // player 2 here
 
-  $("button#playerRoll1").click(function() {
-
-    newRoll1.rollDice();
-
-    $("#rollOne1").text(newRoll1.currentRoll);
-    $("#turnTotal1").text(newRoll1.turn);
-
-  });
-
-  $("button#playerHold1").click(function() {
-
-    newRoll1.totalScore();
-
-    $("#playerTwoScore").text(newRoll1.gameScore);
-
-    $(".playerTwo").hide();
-    $(".playerOne").show();
-  });
-
-
-
-
-});
+//   $("button#playerRoll1").click(function() {
+//
+//     player2NewRoll.rollDice();
+//
+//     $("#rollOne1").text(player2NewRoll.currentRoll);
+//     $("#turnTotal1").text(player2NewRoll.turn);
+//
+//   });
+//
+//   $("button#playerHold1").click(function() {
+//
+//     player2NewRoll.totalScore();
+//
+//     $("#playerTwoScore").text(player2NewRoll.gameScore);
+//
+//   });
+//
+//
+//
+//
+// });
